08c98b7217e0380583367027168f4faca08e43df
xsu <k.kosonogova@xlab-game.com>
2023-10-31 18:11:35 +0300

    first steps

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ShadowChimera
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset m_inputActionAsset;
+        [SerializeField] private CharacterController m_characterController;
+        [SerializeField] private float m_speedMove = 5f;
+
+        private InputActionMap m_playerMap;
+        private InputAction m_moveAction;
+
+
+        private void Awake()
+        {
+            m_playerMap = m_inputActionAsset.FindActionMap("Player");
+            m_moveAction = m_playerMap.FindAction("Move");
+        }
+
+        private void OnEnable()
+        {
+            m_playerMap.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_playerMap.Disable();
+        }
+
+        private void Update()
+        {
+            var move = m_moveAction.ReadValue<Vector2>();
+            if(move != Vector2.zero)
+            {
+                var dir = new Vector3(move.x,0, move.y);
+                m_characterController.SimpleMove(dir * m_speedMove);
+            }
+        }
+    }
+}
